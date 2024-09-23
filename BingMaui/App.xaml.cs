5ab62dda88ceaf981b4620ecfafa1cc244a772da
ChrMonMor <97163196+ChrMonMor@users.noreply.github.com>
2024-09-23 09:19:48 +0200

    Add project files.

---
+++ BingMaui/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BingMaui {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
